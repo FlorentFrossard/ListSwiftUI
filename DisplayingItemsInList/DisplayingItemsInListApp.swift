70ea461cbec9fc0debe1f32952aebbba8078f605
Mohammad Azam <azamsharp@gmail.com>
2021-07-28 14:38:38 -0500

    Initial Commit

---
+++ DisplayingItemsInList/DisplayingItemsInListApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayingItemsInListApp.swift
+//  DisplayingItemsInList
+//
+//  Created by Mohammad Azam on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DisplayingItemsInListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
